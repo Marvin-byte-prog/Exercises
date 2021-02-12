50631423bd92e17c1e2b263d61a81f9e41e81d8c
unknown <you@example.com>
2021-02-12 15:09:08 -0600

    arreglo de nombres

---
+++ Clases/clsArray.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cola.Clases
+{
+    class clsArray
+    {
+        private string nombre;
+        public clsArray(string nombre)
+        {
+            this.nombre = nombre;
+        }
+        public String mostrar()
+        {
+            
+            return nombre;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using Cola.Clases;
+using System;
+
+namespace Cola
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nombre;
+            int numNombres;
+
+            Console.WriteLine("Cantidad de nombres a ingresar: ");
+            numNombres = Convert.ToInt32(Console.ReadLine());
+            Console.Clear();
+            clsArray[] nombres = new clsArray[numNombres];
+            
+            for(int i = 0; i < nombres.Length; i++)
+            {
+                Console.WriteLine("Ingrese el nombre de la persona No. {0}", i + 1);
+                nombre = Console.ReadLine();
+
+                nombres[i] = new clsArray(nombre);
+            }
+
+            Console.Clear();
+
+            Console.WriteLine("Orden en el que entraron las personas.");
+            for (int i = 0; i < nombres.Length; i++)
+            {
+                Console.WriteLine(nombres[i].mostrar());
+            }
+        }
+
+    }
+    
+}
+
+ 
